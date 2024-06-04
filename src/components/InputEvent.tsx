ab8fc1c472f9afb569fe079d1996c4d4f7b8e51e
Teo Hughes <th1522@ic.ac.uk>
2024-06-04 11:08:58 +0100

    Added Teams

--- src/components/InputEvent.tsx
+++ src/components/InputEvent.tsx
@@ -1,5 +1,5 @@
-import { HStack, IconButton, Input } from "@chakra-ui/react";
-import { useState, useRef } from "react";
+import { HStack, IconButton, Input, Select } from "@chakra-ui/react";
+import { useState, useRef, SetStateAction } from "react";
 import { FaCirclePlus } from "react-icons/fa6";
 import { Event } from "../pages/CreatePage";
 import axios from "axios";
@@ -12,12 +12,17 @@ interface Props {
 const InputEvent = ({ onAdd, competitionId }: Props) => {
     
     const [eventName, setEventName] = useState('');
+    const [eventType, setEventType] = useState('')
     const currentIdRef = useRef(0);
+
+    const handleSelectChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+      setEventType(event.target.value);
+    };
 
     const handleAddClick = async () => {
         await axios.post("http://127.0.0.1:8000/api/events/", {
         age_group: eventName,
-        event_type: "Time",
+        event_type: eventType,
         competition: competitionId
       })
       .then()
@@ -37,6 +42,10 @@ const InputEvent = ({ onAdd, competitionId }: Props) => {
     <HStack>
       <Input placeholder="Add Event" size="md" value={eventName}
         onChange={(e) => setEventName(e.target.value)}/>
+      <Select placeholder='Event Type' size="md" value={eventType} onChange={handleSelectChange}>
+        <option value='Time'>Time</option>
+        <option value='Distance'>Distance</option>
+      </Select>
       <IconButton
         variant="outline"
         colorScheme="teal"
---
+++ src/components/InputTeam.tsx
@@ -0,0 +1,53 @@
+import { HStack, IconButton, Input } from "@chakra-ui/react";
+import { useState, useRef } from "react";
+import { FaCirclePlus } from "react-icons/fa6";
+import { Team } from "../pages/CreatePage";
+import axios from "axios";
+
+interface Props {
+    onAdd: (newTeam: Team) => void;
+    competitionId: number,
+}
+
+const InputTeam = ({ onAdd, competitionId }: Props) => {
+    
+    const [teamName, setTeamName] = useState('');
+    const currentIdRef = useRef(0);
+
+
+    const handleAddClick = async () => {
+        await axios.post("http://127.0.0.1:8000/api/organisations/", {
+        name: teamName,
+        // competitionId: competitionId
+      })
+      .then()
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+      const newTeam: Team = {
+        id: currentIdRef.current++, 
+        name: teamName,
+        competitionId: competitionId
+      };
+      onAdd(newTeam);
+      setTeamName(''); 
+      };
+
+
+  return (
+    <HStack>
+      <Input placeholder="Add Team" size="md" value={teamName}
+        onChange={(e) => setTeamName(e.target.value)}/>
+      <IconButton
+        variant="outline"
+        colorScheme="teal"
+        aria-label="Call Sage"
+        fontSize="20px"
+        icon={<FaCirclePlus />}
+        onClick={handleAddClick}
+      />
+    </HStack>
+  );
+};
+
+export default InputTeam;
