bca3e6efcd8dfefa57b6c95a6c851aebd47b9bd0
Teo Hughes <th1522@ic.ac.uk>
2024-06-04 17:49:06 +0100

    Resolve merge conflicts

--- src/components/InputEvent.tsx
+++ src/components/InputEvent.tsx
@@ -20,7 +20,7 @@ const InputEvent = ({ onAdd, competitionId }: Props) => {
     };
 
     const handleAddClick = async () => {
-        await axios.post(backend + '/events/', {
+        await axios.post("http://127.0.0.1:8000/api" + '/events/', {
         age_group: eventName,
         event_type: eventType,
         competition: competitionId
