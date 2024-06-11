e5748839c1ba602f18094883ac57693695cba09b
Teo Hughes <th1522@ic.ac.uk>
2024-06-11 16:24:03 +0100

    update url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    backend: JSON.stringify('http://127.0.0.1:8000')
+    backend: JSON.stringify('https://athleticscore-89d44187f67a.herokuapp.com')
   }
 })
