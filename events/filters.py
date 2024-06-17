58333ba4977c54c9887c29a741a17d83a43c8f8e
jz7622 <jz7622@ic.ac.uk>
2024-06-17 12:50:12 +0100

    Fix: backend filters

--- events/filters.py
+++ events/filters.py
@@ -1,5 +1,5 @@
 from django_filters import rest_framework as filters
-from .models import Result
+from .models import Result, Competition, Athlete, Event
 
 class ResultFilter(filters.FilterSet):
     athlete_name = filters.CharFilter(field_name="athlete__name", lookup_expr="icontains")
@@ -7,8 +7,7 @@ class ResultFilter(filters.FilterSet):
     event_age_category = filters.CharFilter(field_name="event__age_group", lookup_expr="icontains")
     event_type = filters.CharFilter(field_name="event__event_type", lookup_expr="icontains")
     event_name = filters.CharFilter(field_name="event__event_name", lookup_expr="icontains")
-    competition_name = filters.CharFilter(field_name="event__competition__name", lookup_expr="icontains")
-    competition_date = filters.DateFromToRangeFilter(field_name="event__competition__date")
+    competition_id = filters.NumberFilter(field_name="competition__id")
     result_min = filters.NumberFilter(field_name="value", lookup_expr="gte")
     result_max = filters.NumberFilter(field_name="value", lookup_expr="lte")
 
@@ -16,7 +15,7 @@ class ResultFilter(filters.FilterSet):
         model = Result
         fields = [
             'athlete_name', 'athlete_organisation', 'event_age_category', 'event_type', 
-            'event_name', 'competition_name', 'competition_date', 'result_min', 'result_max'
+            'event_name', 'competition_id', 'result_min', 'result_max'
         ]
 
 class CompetitionFilter(filters.FilterSet):
@@ -24,7 +23,7 @@ class CompetitionFilter(filters.FilterSet):
     competition_date = filters.DateFromToRangeFilter(field_name="date")
 
     class Meta:
-        model = Competition'athlete_name', 'athlete_organisation'
+        model = Competition
         fields = [
             'competition_name', 'competition_date'
         ]
@@ -33,23 +32,21 @@ class EventFilter(filters.FilterSet):
     event_age_category = filters.CharFilter(field_name="age_group", lookup_expr="icontains")
     event_type = filters.CharFilter(field_name="event_type", lookup_expr="icontains")
     event_name = filters.CharFilter(field_name="event_name", lookup_expr="icontains")
-    competition_name = filters.CharFilter(field_name="competition__name", lookup_expr="icontains")
-    competition_date = filters.DateFromToRangeFilter(field_name="competition__date")
+    competition_id = filters.NumberFilter(field_name="competition__id")
 
     class Meta:
         model = Event
         fields = [
-            'event_age_category', 'event_type', 'event_name', 'competition_name', 'competition_date'
+            'event_age_category', 'event_type', 'event_name', 'competition_id'
         ]
 
 class AthleteFilter(filters.FilterSet):
     athlete_name = filters.CharFilter(field_name="name", lookup_expr="icontains")
     athlete_organisation = filters.CharFilter(field_name="team__name", lookup_expr="icontains")
-    competition_name = filters.CharFilter(field_name="team__competition__name", lookup_expr="icontains")
-    competition_date = filters.DateFromToRangeFilter(field_name="team__competition__date")
+    competition_id = filters.NumberFilter(field_name="competition__id")
 
     class Meta:
         model = Athlete
         fields = [
-            'athlete_name', 'athlete_organisation', 'competition_name', 'competition_date'
+            'athlete_name', 'athlete_organisation', 'competition_id'
         ]
